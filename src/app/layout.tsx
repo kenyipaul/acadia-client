0509d24e3b296442240193bc621fc7edcec99d97
Kenyi Paul <kenyipaul69@gmail.com>
2025-12-08 21:53:45 +0200

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,10 @@
+import "@/styles/index.css"
+import { ReactNode } from "react";
+
+export default function RootLayout({children}: {children: ReactNode}) {
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    )
+}
