d6d186a061b53ee9f9b6f8512275039fe78013e2
Kenyi Paul <kenyipaul69@gmail.com>
2025-12-09 10:45:49 +0200

    feat: Add custom fonts and form styling
    This commit introduces custom Roboto fonts (Bold, Condensed, Regular) and new styling for forms.
    
    Changes include:
    - Added RobotoBold.ttf, RobotoCondensed.ttf, RobotoRegular.ttf to public/assets/fonts
    - Created src/styles/form.css for login form styles
    - Modified src/app/page.tsx to integrate a login form
    - Updated src/styles/index.css to incorporate overall styling changes with new color palettes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,31 @@
-export default function Home() {
+"use client"
+import Link from "next/link";
+
+export default function Page() {
     return (
-        <main>
-            <h1>Hello, run!</h1>
-        </main>
+        <main className="flex items-center justify-center h-dvh overflow-hidden px-[min(5rem,5%)]">
+
+			<form id="login-form">
+				<div className="mb-5">
+					<h1 className="text-4xl font-roboto-bold">Welcome back</h1>
+					<p className="text-dark-theme-100">Welcome back! Please enter your credentials.</p>
+				</div>
+
+				<div className="input-area">
+					<label htmlFor="id">Roll Number / Staff Id</label>
+					<input type="text" id="id" />
+				</div>
+
+				<div className="input-area">
+					<label htmlFor="password">Password</label>
+					<input type="password" id="password" />
+				</div>
+
+				<p>Forgot your password? <Link className="font-medium text-info-400 underline underline-offset-6" href="/reset" >Reset here</Link></p>
+
+                <button type="submit" className="bg-dark-theme-300 text-white p-4 cursor-pointer rounded-[.7rem]">Sign In</button>
+			</form>
+
+		</main>
     )
 }
