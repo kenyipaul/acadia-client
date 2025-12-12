1fb5ec853c1165379f5ec21fbb8e2fdd43750fbb
blade19791 <ciecjames3@gmail.com>
2025-12-12 20:24:43 +0200

    add styles for secretary in src/app(admin)/secretary/page.tsx

--- src/app/(admin)/secretary/page.tsx
+++ src/app/(admin)/secretary/page.tsx
@@ -1,7 +1,21 @@
+import "@/styles/secretary.css"
+
 export default function Secretary() {
   return (
     <main className="p-2">
-      <h1>HELLO Secretary</h1>
+     <div className="wrapper">
+       <h1 className="header">Secretary Dashboard</h1>
+       <p className="headerDis">Manage students records and school communications.</p>
+
+       
+       <div className="summaryData">
+          <div className="summary">New adminssion</div>
+          <div className="summary">Total students</div>
+          <div className="summary">Students per class</div>
+          <div className="summary">Notice published</div>
+       </div>
+     </div>
+      
     </main>
   );
 }
