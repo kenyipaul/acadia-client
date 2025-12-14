fb7a97df2266eb13e17ffb57a275af6bd4ae0017
Kenyi Paul <kenyipaul69@gmail.com>
2025-12-14 13:21:37 +0200

    feat: Added ability to create and fetch employees new file: Implemented employee registration from at src/app/(admin)/admin/employees/new/page.tsx new file: Added employee fetch module to src/domain/employee/new.employee.ts

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,12 +1,17 @@
 // ! PLEASE DON'T REMOVE ANYTHING FROM THIS FILE
-import Sidebar from "@/layouts/sidebar";
+"use client"
 import { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function RootLayout({children}: {children: ReactNode}) {
     return (
         <html lang="en">
             <body>
-                {children}
+                <QueryClientProvider client={queryClient}>
+                    {children}
+                </QueryClientProvider>
             </body>
         </html>
     )
